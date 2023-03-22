1b8b9cb449c280412e35fd8ce0f95323a0586fc6
AngelaGaytan <angela.nayde.four@gmail.com>
2023-03-22 13:19:07 -0600

    Employee, SalaryEmployee & CommissionEmployee Finish

--- ConceptsPOO/ConceptsPOO/Program.cs
+++ ConceptsPOO/ConceptsPOO/Program.cs
@@ -2,13 +2,32 @@
 using ConceptsPOO;
 
 Console.WriteLine("Hello, World!");
-try
+Employee employee1 = new SalaryEmployee()
 {
-	Date date1 = new Date(2023,12, 45);
-    Console.WriteLine(date1);
-}
-catch (Exception error)
+    Id = 1010,
+    FirstName = "Sandra",
+    LastName = "Morales",
+    BirthDate = new Date(1990, 5, 23),
+    HiringDate = new Date(2022,1,15),
+    IsActive = true,
+    Salary = 1815453.45M
+};
+
+Console.WriteLine(employee1);
+
+Employee employee2 = new CommissionEmployee()
 {
-	Console.WriteLine(error.Message);
-}
+    Id = 2010,
+    FirstName = "Patricia",
+    LastName = "Gutierrez",
+    BirthDate = new Date(1995, 5, 23),
+    HiringDate = new Date(2022,2,5),
+    IsActive = true,
+    Sales = 320000000M,
+    CommissionPercentaje = 0.03F
+};
+
+Console.WriteLine(employee2);
+
+
 
