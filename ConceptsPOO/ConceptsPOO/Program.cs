a7b9a56369b0ebeb5525048f5b5b45f0b46f6bdc
AngelaGaytan <angela.nayde.four@gmail.com>
2023-03-22 00:31:03 -0600

    Finish Date Class Gaytan Angela

--- ConceptsPOO/ConceptsPOO/Program.cs
+++ ConceptsPOO/ConceptsPOO/Program.cs
@@ -1,2 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using ConceptsPOO;
+
 Console.WriteLine("Hello, World!");
+try
+{
+	Date date1 = new Date(2023,12, 45);
+    Console.WriteLine(date1);
+}
+catch (Exception error)
+{
+	Console.WriteLine(error.Message);
+}
+
